19eed84ebced7c4921e4d1565ca9c32b07a3d8f4
nidhinagpure <nagpurenidhi8@gmail.com>
2025-10-05 11:14:23 +0530

    fix App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route, } from 'react-router-dom';
+import "./App.css";
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
