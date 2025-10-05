7c86ee1b8912ebe949ba429f82685346e1ce114a
nidhinagpure <nagpurenidhi8@gmail.com>
2025-10-05 19:28:13 +0530

    import homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Routes, Route, } from 'react-router-dom';
 import "./App.css";
 
 import React from 'react'
+import { Home } from './pages/Home/Home';
 
 const App = () => {
   return (
     <>
-    
+     <Home/>
     </>
   )
 }
